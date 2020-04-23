ece1f4ab5a2bc8db776751b07a103caad6241687
Nicolas MOREAU <moreau-nicolas@users.noreply.github.com>
2020-04-23 09:29:00 +0200

    :sparkles: Initial LCD digit scaling support
    {width: 3, height: 2} is now a working scale.

--- convert-to-lcd.js
+++ convert-to-lcd.js
@@ -1,48 +1,58 @@
-function convertToLcd(number) {
-    const digits = lcdDigits(number);
+function convertToLcd(number, scale = {width: 1, height: 1}) {
+    const digits = lcdDigits(number, scale);
     return joinDigitLines(digits);
 }
 
 const ZERO = [
     ' _ ',
     '| |',
-    '|_|'];
+    '|_|',
+];
 const ONE = [
     '   ',
     '  |',
-    '  |'];
+    '  |',
+];
 const TWO = [
     ' _ ',
     ' _|',
-    '|_ '];
+    '|_ ',
+];
 const THREE = [
     ' _ ',
     ' _|',
-    ' _|'];
+    ' _|',
+];
 const FOUR = [
     '   ',
     '|_|',
-    '  |'];
+    '  |',
+];
 const FIVE = [
     ' _ ',
     '|_ ',
-    ' _|'];
+    ' _|',
+];
 const SIX = [
     ' _ ',
     '|_ ',
-    '|_|'];
+    '|_|',
+];
 const SEVEN = [
     ' _ ',
     '  |',
-    '  |'];
+    '  |',
+];
 const EIGHT = [
     ' _ ',
     '|_|',
-    '|_|'];
+    '|_|',
+];
 const NINE = [
     ' _ ',
     '|_|',
-    ' _|'];
+    ' _|',
+];
 const LCD_DIGITS = [
     ZERO,
     ONE,
@@ -56,9 +66,100 @@ const LCD_DIGITS = [
     NINE,
 ];
 
-function lcdDigits(number) {
+const ZERO_WIDTH_3_HEIGHT_2 = [
+    ' ___ ',
+    '|   |',
+    '|   |',
+    '|   |',
+    '|___|',
+];
+const ONE_WIDTH_3_HEIGHT_2 = [
+    '     ',
+    '    |',
+    '    |',
+    '    |',
+    '    |',
+];
+const TWO_WIDTH_3_HEIGHT_2 = [
+    ' ___ ',
+    '    |',
+    ' ___|',
+    '|    ',
+    '|___ ',
+];
+const THREE_WIDTH_3_HEIGHT_2 = [
+    ' ___ ',
+    '    |',
+    ' ___|',
+    '    |',
+    ' ___|',
+];
+const FOUR_WIDTH_3_HEIGHT_2 = [
+    '     ',
+    '|   |',
+    '|___|',
+    '    |',
+    '    |',
+];
+const FIVE_WIDTH_3_HEIGHT_2 = [
+    ' ___ ',
+    '|    ',
+    '|___ ',
+    '    |',
+    ' ___|',
+];
+const SIX_WIDTH_3_HEIGHT_2 = [
+    ' ___ ',
+    '|    ',
+    '|___ ',
+    '|   |',
+    '|___|',
+];
+const SEVEN_WIDTH_3_HEIGHT_2 = [
+    ' ___ ',
+    '    |',
+    '    |',
+    '    |',
+    '    |',
+];
+const EIGHT_WIDTH_3_HEIGHT_2 = [
+    ' ___ ',
+    '|   |',
+    '|___|',
+    '|   |',
+    '|___|',
+];
+const NINE_WIDTH_3_HEIGHT_2 = [
+    ' ___ ',
+    '|   |',
+    '|___|',
+    '    |',
+    ' ___|',
+];
+const LCD_DIGITS_WIDTH_3_HEIGHT_2 = [
+    ZERO_WIDTH_3_HEIGHT_2,
+    ONE_WIDTH_3_HEIGHT_2,
+    TWO_WIDTH_3_HEIGHT_2,
+    THREE_WIDTH_3_HEIGHT_2,
+    FOUR_WIDTH_3_HEIGHT_2,
+    FIVE_WIDTH_3_HEIGHT_2,
+    SIX_WIDTH_3_HEIGHT_2,
+    SEVEN_WIDTH_3_HEIGHT_2,
+    EIGHT_WIDTH_3_HEIGHT_2,
+    NINE_WIDTH_3_HEIGHT_2,
+];
+
+function lcdDigits(number, scale) {
     const decimalDigits = String(number).split('').map(Number);
-    return decimalDigits.map(decimalDigit => LCD_DIGITS[decimalDigit]);
+    return decimalDigits.map(decimalDigit => scaledLcdDigit(decimalDigit, scale));
+}
+
+function scaledLcdDigit(digit, {width, height}) {
+    let digits = LCD_DIGITS;
+    if (width === 3 && height === 2) {
+        digits = LCD_DIGITS_WIDTH_3_HEIGHT_2;
+    }
+    return digits[digit];
 }
 
 function joinDigitLines(digits) {
